9c0bdb481df9b4bc756884cefdf45a762fc1edb2
ucangun <149247682+ucangun@users.noreply.github.com>
2024-09-05 21:34:22 +0200

    Context Api with ts

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,33 @@
-import React, { FC } from "react";
+import React, { FC, createContext } from "react";
 import "./App.css";
 import Person, { HairColor } from "./components/Person";
 
+interface AppCpntextInterface {
+  name: string;
+  age: number;
+  country: string;
+}
+
+const AppContext = createContext<AppCpntextInterface | null>(null);
+
 const App: FC = () => {
+  const contextValue: AppCpntextInterface = {
+    name: "John",
+    age: 30,
+    country: "German",
+  };
+
   return (
-    <div className="App">
-      <Person
-        name="Umut"
-        age={26}
-        email={"ucangun75@gmail.com"}
-        hairColor={HairColor.Blonde}
-      />
-    </div>
+    <AppContext.Provider value={contextValue}>
+      <div className="App">
+        <Person
+          name="Umut"
+          age={26}
+          email={"ucangun75@gmail.com"}
+          hairColor={HairColor.Blonde}
+        />
+      </div>
+    </AppContext.Provider>
   );
 };
 
